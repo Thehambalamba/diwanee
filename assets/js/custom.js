9876f285b857481bf3303ab203605c5593bb3a97
Nikola Pervic <nikolapervic@me.com>
2018-01-02 03:06:13 +0100

    Menu and tablet
    Did the menu scrolling. Worked on header for tablet versions.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,43 @@
 function openNav() {
-    var x = document.getElementById("main-nav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
+    var x = document.getElementById('main-nav');
+    if (x.className === 'topnav') {
+        x.className += ' responsive';
     } else {
-        x.className = "topnav";
+        x.className = 'topnav';
     }
 }
+
+function menuScroll(e) {
+    var scrollToo = e.target.dataset.position;
+    switch (scrollToo) {
+        case 'footer':
+            var element = document.getElementById('footer');
+                 element.scrollIntoView({
+                     behavior: 'smooth',
+                     block: 'end'
+                 });
+            break;
+        case 'jobs':
+            var element = document.getElementById('jobs');
+                 element.scrollIntoView({
+                     behavior: 'smooth',
+                     block: 'start'
+                 });
+            break;
+        case 'about':
+            var element = document.getElementById('about');
+                 element.scrollIntoView({
+                     behavior: 'smooth',
+                     block: 'start'
+                 });
+            break;
+        default :
+            var element = document.querySelector('body');
+            element.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+    }
+}
+
+
